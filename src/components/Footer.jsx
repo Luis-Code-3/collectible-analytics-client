6b297433ed8a4d3e3a35b94008adab7cdf78d6e3
Luis Cordova <ageofculturebiz@gmail.com>
2023-04-04 17:57:46 -0400

    format navbar and footer with styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from "./footer.module.css"
 
 
 function Footer() {
@@ -6,7 +7,51 @@ function Footer() {
 
 
     return (
-      <div>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerProfile}>
+          <div className={styles.logo}>
+            <div></div>
+            <h1>CollectData</h1>
+          </div>
+          <div className={styles.footerFollow}>
+            <p>Follow Us</p>
+            <a href="https://www.instagram.com/">Instagram</a>
+            <a href="https://twitter.com/">Twitter</a>
+          </div>
+
+        </div>
+
+        <div className={styles.footerRoutes}>
+          <h4>Collectibles</h4>
+          <Link to={'/trading-cards'}>Trading Cards</Link>
+          <Link to={'/sports-cards'}>Sports Cards</Link>
+          <Link to={'/video-games'}>Video Games</Link>
+          <Link to={'/manga'}>Manga</Link>
+        </div>
+
+        <div className={styles.footerNews}>
+          <h4>About</h4>
+          <Link to={'/terms-of-service'}>Terms & Conditions</Link>
+          <Link to={'/privacy'}>Privacy</Link>
+          <Link to={'/faq'}>FAQ</Link>
+          <Link to={'/creator'}>Creator</Link>
+        </div>
+
+        <div className={styles.footerResources}>
+          <h4>Resources</h4>
+          <a href="">Source</a>
+          <a href="">Source</a>
+          <a href="">Source</a>
+          <a href="">Source</a>
+        </div>
+
+        <div className={styles.footerTools}>
+          <h4>Built With</h4>
+            <a href="https://reactjs.org/">React</a>
+            <a href="https://expressjs.com/">Express</a>
+            <a href="https://www.mongodb.com/">MongoDB</a>
+            <a href="https://sass-lang.com/">Sass</a>
+        </div>
   
       </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from './navbar.module.css'
 
 
 function Navbar() {
@@ -6,9 +7,17 @@ function Navbar() {
 
 
     return (
-      <div>
+      <nav className={styles.navbarContainer}>
+        <div className={styles.logo}></div>
+        <h1 className={styles.logoTitle}>CollectData</h1>
+        <div className={styles.searchBar}></div>
+        <Link to={'/about'} className={styles.regLinks}>About</Link>
+        <Link to={'/contact'} className={styles.regLinks}>Contact</Link>
+        <Link to={'/blog'} className={styles.regLinks}>Blog</Link>
+        <Link to={'/login'} className={styles.regLinks}>Log In</Link>
+        <Link to={'/signup'} className={styles.register}>Register</Link>
   
-      </div>
+      </nav>
     );
   }
   
